fa2a76d81323d0ea16d273c9c985947ce8425176
Sreyaash <33704504+droidmx@users.noreply.github.com>
2018-06-05 14:26:43 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,9 @@ const fs = require("fs");
 const ms = require("ms");
 let test = JSON.parse(fs.readFileSync('./test.json', 'utf8'));
 let test2 = JSON.parse(fs.readFileSync('./test2.json', 'utf8'));
-
+const answers = [
+  'Without a doubt', 'Extremely likely', 'Perhaps', 'Maybe', 'I\'ll have to think about that', 'Not a chance!'
+]
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -161,13 +163,142 @@ if (!test[nkilled]) {
 			test[nkilled] = 0
 		}	
 	}	
+		
+		
+		
+		// SENTRY SECTION BEGINS
+		/*
+	    _	.          _ _                   
+| |        | | |                  
+| |__   ___| | |  _   _  ___  ___ 
+| '_ \ / _ \ | | | | | |/ _ \/ __|
+| | | |  __/ | | | |_| |  __/\__ \
+|_| |_|\___|_|_|  \__, |\___||___/
+                   __/ |          
+                  |___/           
+		*/
+		
+		if (msg.content.startsWith(prefix + '1100354821sentry')) {
+	
+	// ACTUAL NEST: <:nest:384850069062418433>
+	let acounterxd = test[sen]
+	const abihluh = msg.channel.send({
+		embed: {
+		color: 0x00FFFF,
+            author: {
+                name: `The Forgotten Foes`,
+                icon_url: client.user.avatarURL
+            },
+            fields: [{
+                    name: "<:sentry:384850040612585482> Sentries Killed: ",
+                    value: `${acounterxd}`,
+                    
+                }
+               
+            ],
+            footer: {
+                text: "Bot coded by ~Droid~#5799",
+            },
+            thumbnail: {
+                url: "https://i.imgur.com/WeGhVJI.png"
+}
+		
+		}
+	})
+	const anpending = await abihluh
+	console.log(anpending.id)
+	if (!test[senid]) {
+			test[senid] = anpending.id
+		}else{
+			test[nestid] = anpending.id
+		}
+		
+}
+		if (msg.content.startsWith(prefix + 'callsentry')) {
+		let anestloc = args
+		if (!msg.member.roles.some(r => ["Trial Farmer", "Certified Farmer"].includes(r.name))) return; 
+		const aidefk = nchannel.send(`@here A Nest has been called by ${msg.author}! 
+\n ***Location:***  ${anestloc} \n React with <:sentry:384850040612585482> if you are coming!`)
+		
+		const amessageeditid = await aidefk
+		amessageeditid.react('384850069062418433')
+		console.log(amessageeditid.id)
+if (!test[skilled]) {
+			test[skilled] = amessageeditid.id
+		}else{
+			test[skilled] = amessageeditid.id
+		}
+		
+		
+		}
+	if (msg.content === prefix + 'sentrydied')  {
+		if (!msg.member.roles.some(r => ["Trial Farmer", "Certified Farmer"].includes(r.name))) return;  
+												       
+		let aupdatedcounter = test[sen] + 1
+		if (!test[sen]) {
+			test[sen] = aupdatedcounter
+		}else{
+			test[sen] = aupdatedcounter
+		}
+		if (test[skilled] === 0) {
+			msg.reply('There is no Active Sentry call!')
+		}
+		let afuckme = test[skilled]
+		let aohhyes = `${afuckme}`
+		let afuckmeharder = nchannel.fetchMessage(aohhyes).then(aohhhh=> aohhhh.delete() )
+		let afuck = test[senid]
+		let amsgidupdated = `${afuck}`
+		console.log(aupdatedcounter)
+			
+			let axddd = nchannel.fetchMessage(amsgidupdated).then(aasd=> {
+		aasd.edit({
+		embed: {
+		color: 0x00FFFF,
+            author: {
+                name: `The Forgotten Foes`,
+                icon_url: client.user.avatarURL
+            },
+            fields: [{
+                    name: "<:sentry:384850040612585482> Sentries Killed: ",
+                    value: `${updatedcounter}`,
+                    
+                }
+               
+            ],
+            footer: {
+                text: "Bot coded by ~Droid~#5799",
+            },
+            thumbnail: {
+                url: "https://i.imgur.com/WeGhVJI.png"
+}
+		
+		}
+	})
+		})
+		if (!test[skilled]) {
+			test[skilled] = 0
+		}else{
+			test[skilled] = 0
+		}	
+	}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		//END HERE ================================================================================
  if (msg.content.startsWith(prefix + 'ping')) {
     msg.channel.send("Pinging... :signal_strength:").then(sent => {
       sent.edit(`:ping_pong: Pong! | Time Taken: ${sent.createdTimestamp - msg.createdTimestamp}ms`)
     })
 }
 		if (msg.content.startsWith(prefix + "verify")) {
-		if (msg.member.roles.some(r => ["Verified Member"].includes(r.name))) {
+		if (msg.member.roles.some(r => ["Members"].includes(r.name))) {
             msg.author.send("You are already verified!")
             msg.delete();
             return;
@@ -205,7 +336,7 @@ if (!test[msg.author.id]) {
                             },
                             {
                                 name: `Place your verification code on any line of your description, but __*it must be the only piece of text on that line.*__`,
-                                value: "Once you have placed the code, type `done` in #verify",
+                                value: "Once you have placed the code, type `done` in <#452260013944209419>",
                             },
                         ],
                         footer: {
@@ -219,8 +350,8 @@ if (!test[msg.author.id]) {
 
 		}
 		
-		if (msg.content.startsWith(prefix + "done")) {
-			 if (msg.member.roles.some(r => ["Shatters"].includes(r.name))) 
+		if (msg.content.startsWith("done")) {
+			 if (msg.member.roles.some(r => ["Members"].includes(r.name))) 
             return;
         let userdatadone = test[msg.author.id]
         if (!userdatadone) {
@@ -259,15 +390,15 @@ if (!test[msg.author.id]) {
 
                         if (rdesc.includes(codexd))
                             msg.guild.member(msg.author).setNickname(`${rname}`)
-                        let lelxdppebtw = msg.guild.roles.find("name", "Verified Member");
+                        let lelxdppebtw = msg.guild.roles.find("name", "Members");
                         // id wasnt working some times, 433784738998910977
                         msg.guild.member(msg.author).addRole(lelxdppebtw.id)
                         msg.author.send("You have successfully been verified!");
-                        msg.guild.channels.find("name", "logging").send({
+                        msg.guild.channels.find("name", "verification-logs").send({
                             embed: {
                                 color: 0xfb7ae4,
                                 author: {
-                                    name: `User Verified | <@${msg.author.tag}>`,
+                                    name: `User Verified | ${ignxd}`,
                                     icon_url: msg.author.avatarURL
                                 },
                                 fields: [{
@@ -336,7 +467,7 @@ if (msg.content.startsWith(prefix + 'ban')) {
     .addField("Responsible Moderator:", `${msg.author.tag} (${msg.author.id})\n\nReason: ${reason}`)
     .setTimestamp(new Date(msg.createdTimestamp))
 
-    msg.guild.channels.find("name", "logging").send({embed});
+    msg.guild.channels.find("name", "mod-logs").send({embed});
   }
 
   if (msg.content.startsWith(prefix + 'kick')) {
@@ -360,34 +491,34 @@ if (msg.content.startsWith(prefix + 'ban')) {
     .addField("Responsible Moderator:", `${msg.author.tag} (${msg.author.id})\n\nReason: ${reason}`)
     .setTimestamp(new Date(msg.createdTimestamp))
 
-    msg.guild.channels.find("name", "logging").send({embed});
+    msg.guild.channels.find("name", "mod-logs").send({embed});
   }
 
   
 if (msg.content.startsWith(prefix + 'info')) {
-  client.channel.send("Bot coded by Droid")
+  client.channel.send("Bot coded by Droid, join his discords! \n")
 }
   
-if (msg.content.startsWith(prefix + 'suspend')) {
+if (msg.content.startsWith(prefix + 'mute')) {
 	var argsss = msg.content.split(" ");
-if (!msg.member.hasPermission("ADMINISTRATOR"))
-                return msg.reply(":x: Sorry, you don't have permissions to use this!");
+if (!msg.member.hasPermission("MANAGE_ROLES_OR_PERMISSIONS"))
+                return msg.reply(":x: Sorry, you don't have permission to use this!");
           let member2 = msg.mentions.members.first();
           if(!member2) return msg.reply(":x: " + "| You need to mention a user/member!");
-          let muteRole2 = msg.guild.roles.find("name", "Suspended");
-          if(!muteRole2) return msg.reply(":x: " + "| You do not have the \"Suspended\" role created!");
+          let muteRole2 = msg.guild.roles.find("name", "Muted");
+          if(!muteRole2) return msg.reply(":x: " + "| You do not have the \"Muted\" role created!");
           let time2 = argsss[2];
           let reasonxd = argsss[3];
-            if(!reasonxd) return msg.reply(":x: " + "| Please provide a reason for the suspension!");
+            if(!reasonxd) return msg.reply(":x: " + "| Please provide a reason for the mute!");
           if(!time2) {
-            msg.channel.send("Please provide a time for the suspension!");
+            msg.channel.send("Please provide a time for the mute!");
           }else {
             member2.addRole(muteRole2.id);
-            msg.channel.send(member2 + ` has been suspended for: ${ms(ms(time2), {long: true})}, Reason: ${reasonxd}!`);
+            msg.channel.send(member2 + ` has been muted for: ${ms(ms(time2), {long: true})}, Reason: ${reasonxd}!`);
 
             setTimeout(function(){
               member2.removeRole(muteRole2.id);
-              msg.channel.send(member2 + ` has been unsuspended, suspension lasted for ${ms(ms(time2), {long: true})}`)
+              msg.channel.send(member2 + ` has been unmuted, suspension lasted for ${ms(ms(time2), {long: true})}`)
 
             }, ms(time2));
 
@@ -415,7 +546,193 @@ if (!msg.member.hasPermission("ADMINISTRATOR"))
      .addField("Responsible Moderator:", `${msg.author.tag} (${msg.author.id})\n\nReason: ${reason}`)
      .setTimestamp(new Date(msg.createdTimestamp))
 
-     msg.guild.channels.find("name", "logging").send({embed});
+     msg.guild.channels.find("name", "mod-logs").send({embed});
+}
+		if (msg.content.startsWith(prefix + 'rotmgchars')) {
+			msg.channel.send({
+                embed: {
+                    color: 0x000000,
+                    author: {
+                        name: client.user.username,
+                        icon_url: client.user.avatarURL
+                    },
+                    thumbnail: {
+                        url: 'https://steamuserimages-a.akamaihd.net/ugc/615025248066186198/CCF7A2CA7AAC3180249A4C8E8346C0DA68A4D839/'
+                    },
+                    title: "**Realm Characters**",
+                    description: "These are all of the current Realm of the Mad God characters.",
+                    fields: [{
+                            name: "__**Rogue**__ : Uses a medium ranged dagger. Special ability is cloaking.",
+                            value: "\u200b"
+                        },
+                        {
+                            name: "__**Archer**__ : Uses a long ranged bow. Special ability is shooting debuffs.",
+                            value: "\u200b"
+                        },
+                        {
+                            name: "__**Wizard**__ : Uses a long ranged staff. Special ability is burst of damage within a range.",
+                            value: "\u200b"
+                        },
+                        {
+                            name: "__**Priest**__ : Uses a long ranged wand. Special ability is AoE healing.",
+                            value: "\u200b"
+                        },
+                        {
+                            name: "__**Warrior**__ : Uses a short ranged sword. Special ability is berserk mode.",
+                            value: "\u200b"
+                        },
+                        {
+                            name: "__**Knight**__ : Uses a short ranged sword. Special ability is shield bash.",
+                            value: "\u200b"
+                        },
+                        {
+                            name: "__**Paladin**__ : Uses a short ranged sword. Special ability is AoE buff.",
+                            value: "\u200b"
+                        },
+                        {
+                            name: "__**Assassin**__ : Uses a medium ranged dagger. Special ability is throwing poisons that damage over time.",
+                            value: "\u200b"
+                        },
+                        {
+                            name: "__**Necromancer**__ : Uses a long ranged staff. Special ability is lifesteal.",
+                            value: "\u200b"
+                        },
+                        {
+                            name: "__**Huntress**__ : Uses a long ranged bow. Special ability is placing damaging traps.",
+                            value: "\u200b"
+                        },
+                        {
+                            name: "__**Mystic**__ : Uses a long ranged staff. Special ability is stasising enemies.",
+                            value: "\u200b"
+                        },
+                        {
+                            name: "__**Trickster**__ : Uses a medium ranged dagger. Special ability is sending out decoys.",
+                            value: "\u200b"
+                        },
+                        {
+                            name: "__**Sorcerer**__ : Uses a long ranged wand. Special ability is damage dealt across enemies.",
+                            value: "\u200b"
+                        },
+                        {
+                            name: "__**Ninja**__ : Uses a medium ranged katana. Special ability is shooting damaging shuriken.",
+                            value: "\u200b"
+                        }
+                    ],
+                    timestamp: new Date(),
+                    footer: {
+                        icon_url: client.user.avatarURL,
+                    }
+                }
+});
+			
+		}
+		
+		if (msg.content.startsWith(prefix + 'realmeye')) {
+		let user = args.slice(0).join("");
+           let rapii = "http://www.tiffit.net/RealmInfo/api/user?u=" + user + "&f=c;";
+          
+           message.delete();
+           if(!user)
+return message.channel.send("Please include a username after `!realmeye`.")
+           
+           snekfetch.get(rapii).then(r => {
+let asdesc = r.body.description;
+let asname = r.body.name
+let asstars = r.body.rank
+let aslocation = r.body.last_seen
+let asfame = r.body.fame
+let ascount = r.body.characterCount
+let asacctfame = r.body.account_fame
+let ascreated = r.body.created
+let asskins = r.body.skins
+let asguild = r.body.guild
+
+
+           
+           message.channel.send({embed: {
+  color: 0xfbd27a,
+  author: {
+    name: "Realmeye Info for" + user,
+    icon_url: client.user.avatarURL
+  },
+  fields: [
+      {
+      name: "Description",
+      value: "Desc: " + asdesc,
+      inline: true
+    },
+    {
+      name: "Stars",
+      value: "Stars: " + asstars,
+      inline: true
+    },
+    {
+      name: "Last-seen Location",
+      value: "server: " + aslocation, 
+      inline: true
+    },
+    {
+      name: "Character Fame",
+      value: "Fame: " + asfame, 
+      inline: true
+    },
+           {
+             name: "Account Fame",
+             value: "Fame: " + asacctfame, 
+             inlint: true
+           },
+           {
+             name: "Account Created",
+             value: "Date: " + ascreated,
+             inline: true
+           },
+           {
+             name: "Skin Count",
+             value: "Skins: " + asskins,
+             inline: true
+           },
+           {
+             name: "Guild",
+             value: "Guild:" + asguild,
+             inline: true
+           }
+  ],
+  timestamp: new Date(),
+  footer: {
+    icon_url: "https://cdn.discordapp.com/avatars/160140367554019329/a423acbb3957e25bce788915eda9414a.png?size=2048",
+    text: "~Droid~#5799"
+  }//end
+  }})
+})	
+		}
+		
+		if (msg.content.startsWith(prefix + 'help')) {
+  msg.channel.send(":inbox_tray: | Listed below are some commands")
+  msg.channel.send(`\`\`\`asciidoc
+= General =
+!ping :: Hm. I wonder what this does? /sarcasm
+!8ball :: Ask the magic 8ball a question. Pretty self explanatory
+!help :: Brings up this menu
+!serverinfo :: Shows information about the server
+
+= Moderation =
+!ban :: Bans the user specified
+!kick :: Kicks the user specified
+!softban :: Softbans the specified user
+!mute :: Mutes the user for a specified time period. Example - !mute @droid 5d being a bad boy
+
+= RotMG =
+!realmeye :: Shows basic information about a IGN. Example - !realmeye DroidMXBro
+!rotmgchars :: Shows all the RotMG Characters currently in game.
+
+This bot was coded by ~Droid~#5799\`\`\``)
+}
+		
+		 if (msg.content.startsWith(prefix + '8ball')) {
+  let args = msg.content.split(" ").slice(1);
+  let question = args[0]
+  msg.channel.send(`:8ball: | ${answers[Math.floor(Math.random() * answers.length)]}`);
+  
 }
 });
 
